3de86a3e9455bb0c58f3aa7235e8701a0dcf6a4c
Sharonne24 <sawuor395@gmail.com>
2023-03-24 14:44:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
-}
+
+  
+  function addingEventListener() {
+    const input = document.querySelector("input");
+  
+     input.addEventListener("click", function() {
+      alert("Button clicked!");
+    });
+  }
+  
+  
